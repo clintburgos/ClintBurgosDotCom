ccc6e0db6ce2807d23d05fd3cd3cf0ca22c892f5
Clinton Burgos <clintburgos@users.noreply.github.com>
2018-02-24 12:25:32 -0500

    frontpage text

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,9 +3,11 @@
     <img id="me-banner" src="../assets/images/me_banner.jpg">
     <div id="about">
       <text-block
-        title="I thrive with heavy research and fine-tuning."
-        text="After getting my start as a programmer in neuroscience labs, I took on a series of open-ended projects at Wayfair in which I had generous control of project direction and design...<br><br>
-        Get in contact with me with the social media links at the bottom of the page. Together let’s learn, innovate, and make cool stuff!"
+        title="I thrive with projects that involve research and discovery."
+        text="After getting my start as a programmer in neuroscience labs, I took on a series of open-ended projects at Wayfair in which I had generous control of project direction and design. I've had experience building web/mobile apps, machine learning models, a streaming data pipeline, and more. The part of the process of software development I most enjoy is fine-tuning the results of an application through creative experimentation."
+      />
+      <text-block
+        text="Get in contact with me with the social media links at the bottom of the page. Let’s learn, innovate, and make cool stuff!"
       />
     </div>
   </div>
